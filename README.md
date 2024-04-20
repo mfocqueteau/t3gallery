28b34834c8b019965a8cad108e4c4b5e0245483d
Martín Ocqueteau <martin.ocqueteau@solarityenergia.com>
2024-04-19 22:40:58 -0400

    TopNav y Galería

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## TODO
 
-- Make it deploy (Vercel)
-- Scaffold basic ui with mock data
-- Actually set up a database (Vercel Postgres)
-- Attach database to UI
-- Add authentication (w/ Clerk)
-- Add image upload
-- Error management (w/ Sentry)
-- Routing/image page (parallel route)
-- Delete buttons (w/ Server Actions)
-- Analytics (posthog)
-- Ratelimiting (upstash)
+- [x] Make it deploy (Vercel)
+- [ ] Scaffold basic ui with mock data
+- [ ] Actually set up a database (Vercel Postgres)
+- [ ] Attach database to UI
+- [ ] Add authentication (w/ Clerk)
+- [ ] Add image upload
+- [ ] Error management (w/ Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete buttons (w/ Server Actions)
+- [ ] Analytics (posthog)
+- [ ] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Link from "next/link";
-
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/122fd808-e3cf-4550-a094-bc590990464b-12mlt5.jpg",
+    "https://utfs.io/f/2e093295-5887-4aeb-99ab-f50577c705af-12mlt6.jpg",
+    "https://utfs.io/f/af361213-2d6f-4613-9d66-80f455a1636b-12mlt7.jpg",
+    "https://utfs.io/f/908cd997-8f26-4b12-8f64-00940b4415ea-12mlt8.webp",
+    "https://utfs.io/f/122fd808-e3cf-4550-a094-bc590990464b-12mlt5.jpg",
+    "https://utfs.io/f/2e093295-5887-4aeb-99ab-f50577c705af-12mlt6.jpg",
+    "https://utfs.io/f/af361213-2d6f-4613-9d66-80f455a1636b-12mlt7.jpg",
+    "https://utfs.io/f/908cd997-8f26-4b12-8f64-00940b4415ea-12mlt8.webp",
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery!
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
